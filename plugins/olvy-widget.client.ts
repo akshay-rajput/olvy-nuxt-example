8c53211f835465d82e180e7eb40149fd3a833836
Akshay <akshay@olvy.co>
2023-07-06 09:00:05 +0530

    remove - comments

--- plugins/olvy-widget.client.ts
+++ plugins/olvy-widget.client.ts
@@ -1,9 +1,7 @@
-// import Vue from 'vue'
 import { defineNuxtPlugin } from "#app";
 // @ts-ignore
 import OlvyWidget from "@olvyhq/widget-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // OlvyWidget.install(nuxtApp);
   nuxtApp.vueApp.use(OlvyWidget);
 });
